fd0551bf0c583c40fd1d3391c325d95695a2a193
Adolfo Machado <adolfo.machado76@gmail.com>
2024-04-09 20:25:23 -0300

    Implementando lógica p/verificar se email já existe e numero de máximo de participantes

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -23,17 +23,44 @@ export async function registerForEvent(app: FastifyInstance) {
             }
         }, async (request, reply) => {
             const { eventId } = request.params;
-            const  { name, email } = request.body;
+            const { name, email } = request.body;
 
-            const attendee = await prisma.attendee.create({
-                data: {
-                    name,
-                    email,
-                    eventId
+            const attendeeFromEmail = await prisma.attendee.findUnique({
+                where: {
+                    eventId_email: {
+                        eventId,
+                        email
+                    }
                 }
             })
 
-            return reply.status(201).send({ attendeeId: attendee.id});
+            if (attendeeFromEmail !== null) {
+                throw new Error("Este email já esta registrado para este usuário.");
+            }
+
+            const [event, amountOfAttendeesForEvent] = await Promise.all([
+                prisma.event.findUnique({
+                    where: {
+                        id: eventId
+                    }
+                }),
+
+                prisma.attendee.count({
+                    where: {
+                        eventId
+                    }
+                })
+            ]);
+
+            if (event?.maximumAttendees && amountOfAttendeesForEvent >= event?.maximumAttendees) {
+                throw new Error("Número maximo de participantes atingido.");
+            }
+
+            const attendee = await prisma.attendee.create({
+                data: {name, email, eventId}
+            });
+
+            return reply.status(201).send({ attendeeId: attendee.id });
 
         })
 }

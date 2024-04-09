3600e1db199b5ed2d5d13ca1e313f2679f08a3f7
Adolfo Machado <adolfo.machado76@gmail.com>
2024-04-09 17:10:13 -0300

    Criada a tabela attendees e a rota para cadastrar participante

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import { createEvent } from "./routes/create-even";
+import { registerForEvent } from "./routes/register-for-event";
 
-const app = fastify();
+export const app = fastify();
 const PORT = parseInt(process.env['SERVER_PORT'] || '3333');
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(createEvent)
+app.register(createEvent);
+app.register(registerForEvent);
 
 app.listen({ port: PORT }).then(() => {
     console.log(`Http server running on port:${PORT}`);

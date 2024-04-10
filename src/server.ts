d2a9ae6bf2c18fe6f96f38db791cd3ccba6f84bf
Adolfo Machado <adolfo.machado76@gmail.com>
2024-04-09 21:26:15 -0300

    Criada a rota para encontrar um evento

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod'
 import { createEvent } from "./routes/create-even";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
 
 export const app = fastify();
 const PORT = parseInt(process.env['SERVER_PORT'] || '3333');
@@ -11,6 +12,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
 app.register(registerForEvent);
+app.register(getEvent);
 
 app.listen({ port: PORT }).then(() => {
     console.log(`Http server running on port:${PORT}`);

583b69639090caeae9bdaee097765542bfad84e4
Adolfo Machado <adolfo.machado76@gmail.com>
2024-04-16 22:05:23 -0300

    Criando a rota para buscar os participantes de um evento, permitir filtar um participante

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
 
 export const app = fastify();
 const PORT = parseInt(process.env['SERVER_PORT'] || '3333');
@@ -17,6 +18,7 @@ app.register(registerForEvent);
 app.register(getEvent);
 app.register(getAttendeeBadge);
 app.register(checkIn);
+app.register(getEventAttendees);
 
 app.listen({ port: PORT }).then(() => {
     console.log(`Http server running on port:${PORT}`);

0613358183d5b36d779a30990052fdd961f1744f
Adolfo Machado <adolfo.machado76@gmail.com>
2024-05-21 17:47:29 -0300

    Removendo Migrations antigas

--- prisma/migrations/20240409200215_create_table_attendees/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "attendees" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "envent_id" TEXT NOT NULL,
-    CONSTRAINT "attendees_envent_id_fkey" FOREIGN KEY ("envent_id") REFERENCES "events" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
